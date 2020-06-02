903a650c1c44070434899f93c7d2f34113d734a0
esenliyim <esenliyim@gmail.com>
2020-06-02 20:39:35 +0200

    ditched sp and transitioned to glib.dbus

--- extension.js
+++ extension.js
@@ -3,8 +3,37 @@ const Mainloop = imports.mainloop;
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
+const Variant = imports.gi.GLib.Variant;
 
 let panelButton, panelButtonText, timeout;
+
+const dest = "org.mpris.MediaPlayer2.spotify";
+const path = "/org/mpris/MediaPlayer2";
+
+const spotifyDbus = `<node>
+<interface name="org.mpris.MediaPlayer2.Player">
+    <property name="PlaybackStatus" type="s" access="read"/>
+    <property name="Metadata" type="a{sv}" access="read"/>
+</interface>
+</node>`;
+
+//org.freedesktop.DBus.Properties.Get( s:interface_name, s:property_name ) -> ( v:value )
+const freedesktopDbus = `<node>
+<interface name="org.freedesktop.DBus.Properties">
+    <method name="Get">
+        <arg type="s" direction="in" name="interface_name"/>
+        <arg type="s" direction="in" name="property_name"/>
+        <arg type="v" direction="out" name="value"/>
+    </method>
+</interface>
+</node>`;
+
+const spotifyProxyWrapper = Gio.DBusProxy.makeProxyWrapper(spotifyDbus);
+const freedesktopProxyWrapper = Gio.DBusProxy.makeProxyWrapper(freedesktopDbus);
+
+let spotfyProxy = spotifyProxyWrapper(Gio.DBus.session, dest, path);
+let freedesktopProxy = freedesktopProxyWrapper(Gio.DBus.session, dest, path);
 
 function init () {
     panelButton = new St.Bin({
@@ -13,7 +42,7 @@ function init () {
 
     panelButtonText = new St.Label({
         style_class : "taskbarPanelText",
-        text: "Yerim yerim yerim",
+        text: "🍔",
         y_align : Clutter.ActorAlign.CENTER
     });
     panelButton.set_child(panelButtonText);
@@ -21,7 +50,7 @@ function init () {
 
 function enable () {
     Main.panel._rightBox.insert_child_at_index(panelButton, 1);
-    timeout = Mainloop.timeout_add_seconds(1, setButtonText);
+    timeout = Mainloop.timeout_add_seconds(1, decideText);
 }
 
 function disable () {
@@ -29,22 +58,46 @@ function disable () {
     Main.panel._rightBox.remove_child(panelButton);
 }
 
-function setButtonText () {
+function decideText () {
 
-    var [ok, out, err, exit] = GLib.spawn_command_line_sync('./.sp.sh current');
+    //var [ok, out, err, exit] = GLib.spawn_command_line_sync('./.sp.sh current');
 
-    out = out.toString();
+    //out = out.toString();
 
-    if (out.includes("Error: ")) {
-        panelButtonText.set_text("");
+  /*  if (out.includes("Error: ")) {
+        setButtonText("");
+        return true;
     } else {
+*/
+        let status = spotfyProxy.PlaybackStatus;
+        let metadata = spotfyProxy.Metadata;
 
+        if (status == "Paused") {
+            setButtonText("⏸️ Paused");
+        } else {
+            let title = metadata['xesam:title'].get_string()[0];
+            let artist = metadata['xesam:albumArtist'].get_strv()[0];
+            //log(title + " | " + artist);
+            //setButtonText(metadata['xesam:title'].value);
+            let output = "🧑‍🦲 " + artist + " 🎶 " + title;
+            setButtonText(output);
+        }
+
+        return true;
+
+        /*
         let artist = out.substring(out.indexOf("AlbumArtist") + 11,
             out.indexOf("\n", out.indexOf("AlbumArtist"))).trim();
         let track = out.substring(out.indexOf("Title") + 5).trim();
 
         panelButtonText.set_text(artist + " | " + track);
-    }
-    
-    return true;
+        */
+//    }
+
+}
+
+function setButtonText (output) {
+
+    panelButtonText.set_text(output);
+
 }
