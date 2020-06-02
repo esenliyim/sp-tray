dcf80edb3285abbfb12ec1e2706041f99e6af015
esenliyim <esenliyim@gmail.com>
2020-06-02 15:17:01 +0200

    bek tu sp

--- extension.js
+++ extension.js
@@ -2,13 +2,7 @@ const St = imports.gi.St;
 const Mainloop = imports.mainloop;
 const Main = imports.ui.main;
 const Clutter = imports.gi.Clutter;
-
-var dest = "org.mpris.MediaPlayer2.spotify"
-var path = "/org/mpris/MediaPlayer2"
-var iface = "org.mpris.MediaPlayer2.Player"
-var propertiesIface = "org.freedesktop.DBus.Properties"
-
-var dbus = require('dbus-next');
+const GLib = imports.gi.GLib;
 
 let panelButton, panelButtonText, timeout;
 
@@ -19,7 +13,7 @@ function init () {
 
     panelButtonText = new St.Label({
         style_class : "taskbarPanelText",
-        text : "Yerim yerim yerim",
+        text: "Yerim yerim yerim",
         y_align : Clutter.ActorAlign.CENTER
     });
     panelButton.set_child(panelButtonText);
@@ -35,15 +29,15 @@ function disable () {
     Main.panel._rightBox.remove_child(panelButton);
 }
 
-function setButtonText () {
+async function setButtonText () {
 
-    let obj = await dbus.getProxyObject(dest, path);
-    let props = obj.getInterface(propertiesIface);
-    let metadata = await props.Get(iface, 'Metadata');
-    
-    let artist = metadata.value['xesam:artist'] ? metadata.value['xesam:artist'].value : 'unknown';
-    let title = metadata.value['xesam:title'] ? metadata.value['xesam:title'].value : 'unknown';
-    
-    panelButtonText.set_text(artist + " - " + title);
+    var [ok, out, err, exit] = GLib.spawn_command_line_sync('./sp.sh current');
+    out = out.toString();
+
+    let artist = out.subString(out.indexOf("AlbumArtist") + 11,
+     out.indexOf("\n", out.indexOf("AlbumArtist"))).trim();
+    let track = out.subString(out.indexOf("Title")).trim();
+
+    panelButtonText.set_text(out.toString());
     return true;
 }
